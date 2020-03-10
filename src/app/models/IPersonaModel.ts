97c71a4fd8630d039ebf671c671a6ce713c0bb6c
bertol26 <analista8@domiruth.com>
2020-03-10 15:20:21 -0500

    segundo commit

--- src/app/models/IPersonaModel.ts
+++ src/app/models/IPersonaModel.ts
@@ -1,3 +1,15 @@
 export class IPersonaModel {
-    
+    ruc: string;
+    razonsocial: string;
+    estadocontribuyente: string;
+    tipovia: string;
+    interior: string;
+    manzana: string;
+    condiciondomicilio: string;
+    nombrevia: string;
+    lote: string;
+    kilometro: string;
+    numero: string;
+    departamento: string;
+    ubigeo: string;
 }
