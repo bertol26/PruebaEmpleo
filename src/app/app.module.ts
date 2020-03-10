9761762b7839c6b8c8e4cbd0df03ebf069bb6c20
bertol26 <analista8@domiruth.com>
2020-03-10 13:29:48 -0500

    primera subida

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { LoginComponent } from './components/login/login.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistroComponent,
+    LoginComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule,
